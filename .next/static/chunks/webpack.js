6a5301b35ac2906f4bbbad792ad9982a65f267ea
Nwachukwu Promise <treyp99>
2022-09-25 15:31:21 +0000

    new push footer

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "6e0998ef36ee6411"; }
+/******/ 		__webpack_require__.h = function() { return "a5e1026852ec044a"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/layout/Footer.js
+++ components/layout/Footer.js
@@ -51,7 +51,7 @@ export const Footer = () => {
         </div>
 
         <div className="space-y-2 text-sm sm:col-span-2">
-          <p className="tracking-wide text-sm text-gray-800">
+          <p className="text-base font-bold tracking-wide text-gray-800">
             Jobs
           </p>
           <div className="">
@@ -107,7 +107,7 @@ export const Footer = () => {
           <div className="flex items-center mt-1 space-x-3">
             <Link href="https://twitter.com/Droom__work?t=KQdoCJ4aRxXFaoRTCgu0oA&s=08/">
             <a
-              className="text-gray-500 transition-colors duration-300 hover:text-deep-purple-accent-400"
+              className="text-gray-800 transition-colors duration-300 hover:text-[#F49038]"
             >
               <svg viewBox="0 0 24 24" fill="currentColor" className="h-5">
                 <path d="M24,4.6c-0.9,0.4-1.8,0.7-2.8,0.8c1-0.6,1.8-1.6,2.2-2.7c-1,0.6-2,1-3.1,1.2c-0.9-1-2.2-1.6-3.6-1.6 c-2.7,0-4.9,2.2-4.9,4.9c0,0.4,0,0.8,0.1,1.1C7.7,8.1,4.1,6.1,1.7,3.1C1.2,3.9,1,4.7,1,5.6c0,1.7,0.9,3.2,2.2,4.1 C2.4,9.7,1.6,9.5,1,9.1c0,0,0,0,0,0.1c0,2.4,1.7,4.4,3.9,4.8c-0.4,0.1-0.8,0.2-1.3,0.2c-0.3,0-0.6,0-0.9-0.1c0.6,2,2.4,3.4,4.6,3.4 c-1.7,1.3-3.8,2.1-6.1,2.1c-0.4,0-0.8,0-1.2-0.1c2.2,1.4,4.8,2.2,7.5,2.2c9.1,0,14-7.5,14-14c0-0.2,0-0.4,0-0.6 C22.5,6.4,23.3,5.5,24,4.6z" />
@@ -117,7 +117,7 @@ export const Footer = () => {
 
             <Link href="https://www.instagram.com/droom__work/">
             <a
-              className="text-gray-500 transition-colors duration-300 hover:text-deep-purple-accent-400"
+              className="text-gray-800 transition-colors duration-300 hover:text-[#F49038]"
             >
               <svg viewBox="0 0 30 30" fill="currentColor" className="h-6">
                 <circle cx="15" cy="15" r="4" />
@@ -137,13 +137,13 @@ export const Footer = () => {
             </Link>
 
           </div>
-          <p className="mt-4 text-sm text-gray-500">
+          <p className="mt-4 text-sm text-gray-800">
             Stay conneced with droomwork.io on your social accounts.
           </p>
         </div>
       </div>
       <div className="flex flex-col-reverse justify-between pt-5 pb-10 border-t lg:flex-row">
-        <p className="text-sm text-gray-600">
+        <p className="text-sm text-gray-800">
           © Copyright {new Date().getFullYear()} Droomwork. All rights reserved.
         </p>
         <ul className="flex flex-col mb-3 space-y-2 lg:mb-0 sm:space-y-0 sm:space-x-5 sm:flex-row">
